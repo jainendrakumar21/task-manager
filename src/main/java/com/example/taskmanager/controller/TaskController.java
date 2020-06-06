b563b5f69bc63fc1930132f7d67ec96d9cfc546d
Jainendra Kumar <M1049242@mindtree.com>
2020-06-06 13:41:59 +0530

    html for everyone added

--- src/main/java/com/example/taskmanager/controller/TaskController.java
+++ src/main/java/com/example/taskmanager/controller/TaskController.java
@@ -1,15 +1,13 @@
 package com.example.taskmanager.controller;
 
-import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,14 +16,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.taskmanager.model.Error;
 import com.example.taskmanager.model.ServiceResponse;
 import com.example.taskmanager.model.Task;
 import com.example.taskmanager.repo.TaskRepository;
 
-@RestController
+@Controller
 @RequestMapping("/tasks")
 public class TaskController {
 	@Autowired
@@ -34,7 +32,7 @@ public class TaskController {
 	private TaskRepository taskrepo;
 
 	@GetMapping
-	public ServiceResponse<List<Task>> getAllTasks() {
+	public @ResponseBody ServiceResponse<List<Task>> getAllTasks() {
 		final ServiceResponse<List<Task>> response = new ServiceResponse<>();
 		final List<Task> tasks = taskrepo.findAll();
 		if (!CollectionUtils.isEmpty(tasks)) {
@@ -50,7 +48,7 @@ public class TaskController {
 	}
 
 	@GetMapping("/{taskId}")
-	public ServiceResponse<Task> getByTaskId(@PathVariable Integer taskId) {
+	public @ResponseBody ServiceResponse<Task> getByTaskId(@PathVariable Integer taskId) {
 		final ServiceResponse<Task> response = new ServiceResponse<>();
 		Optional<Task> taskOptional = taskrepo.findById(taskId);
 		if (taskOptional.isPresent()) {
@@ -65,7 +63,7 @@ public class TaskController {
 	}
 
 	@PostMapping("/save")
-	public ServiceResponse<Task> saveTask(@RequestBody Task task) {
+	public @ResponseBody ServiceResponse<Task> saveTask(@RequestBody Task task) {
 		final ServiceResponse<Task> response = new ServiceResponse<>();
 		taskrepo.save(task);
 		response.setData(task);
@@ -73,7 +71,7 @@ public class TaskController {
 	}
 
 	@PutMapping("/update/{taskId}")
-	public ServiceResponse<Task> updateTask(@PathVariable Integer taskId, @RequestBody Task newTask) {
+	public @ResponseBody ServiceResponse<Task> updateTask(@PathVariable Integer taskId, @RequestBody Task newTask) {
 		final ServiceResponse<Task> response = new ServiceResponse<>();
 		Optional<Task> taskOptional = taskrepo.findById(taskId);
 		if (taskOptional.isPresent()) {
@@ -92,7 +90,7 @@ public class TaskController {
 	}
 
 	@DeleteMapping("/delete/{taskId}")
-	public ServiceResponse<Task> deleteTask(@PathVariable Integer taskId) {
+	public @ResponseBody ServiceResponse<Task> deleteTask(@PathVariable Integer taskId) {
 		final ServiceResponse<Task> response = new ServiceResponse<>();
 		Optional<Task> taskOptional = taskrepo.findById(taskId);
 		if (taskOptional.isPresent()) {
@@ -108,45 +106,54 @@ public class TaskController {
 		return response;
 	}
 
-	@GetMapping("/welcome")
-	public ServiceResponse<Map<String, String>> welcomeUser() {
-		final ServiceResponse<Map<String, String>> response = new ServiceResponse<>();
-		Map<String, String> headerDetails = getRequestHeadersInMap(request);
-		response.setData(headerDetails);
-		return response;
-	}
 	@GetMapping("/ip")
-	public String getClientIp() {
+	public @ResponseBody String getClientIp() {
 		String ip = getClientIp(request);
 		return "Your current Public IP address: " + ip;
 	}
 
-	private Map<String, String> getRequestHeadersInMap(HttpServletRequest request) {
+	@GetMapping("/abhishek")
+	public String abhishek() {
+		return "abhishek";
 
-		Map<String, String> result = new HashMap<>();
+	}
 
-		Enumeration<String> headerNames = request.getHeaderNames();
-		while (headerNames.hasMoreElements()) {
-			String key = (String) headerNames.nextElement();
-			String value = request.getHeader(key);
-			System.out.println(value);
-			result.put(key, value);
+	@GetMapping("/dimple")
+	public String dimple() {
+		return "dimple";
+
+	}
+
+	@GetMapping("/neha")
+	public String neha() {
+		return "neha";
+
+	}
+
+	@GetMapping("/ragini")
+	public String ragini() {
+		return "hello";
+
+	}
+
+	@GetMapping("/niku")
+	public String niku() {
+		return "hello";
+
+	}
+
+	private static String getClientIp(HttpServletRequest request) {
+
+		String remoteAddr = "";
+
+		if (request != null) {
+			remoteAddr = request.getHeader("X-FORWARDED-FOR");
+			if (remoteAddr == null || "".equals(remoteAddr)) {
+				remoteAddr = request.getRemoteAddr();
+			}
 		}
 
-		return result;
+		return remoteAddr;
 	}
-	 private static String getClientIp(HttpServletRequest request) {
-
-	        String remoteAddr = "";
-
-	        if (request != null) {
-	            remoteAddr = request.getHeader("X-FORWARDED-FOR");
-	            if (remoteAddr == null || "".equals(remoteAddr)) {
-	                remoteAddr = request.getRemoteAddr();
-	            }
-	        }
-
-	        return remoteAddr;
-	    }
 
 }
